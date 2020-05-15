5c0ca409f1191fb6f4d334b46391f7c9588d56db
Joe Russell <russej9@mail.uc.edu>
2020-05-15 15:01:00 -0400

    Camera controls and button script experimenting

---
+++ Assets/ButtonPress.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject panel;
+    void Start()
+    {
+        panel = GameObject.FindWithTag("UI Component");
+        panel.SetActive(false);
+        Debug.Log("min = " + panel.GetComponent<RectTransform>().rect.min);
+        Debug.Log("max - " + panel.GetComponent<RectTransform>().rect.max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0) && !(panel.activeSelf) && GetComponent<RectTransform>().rect.Contains(Input.mousePosition))
+        {
+            panel.SetActive(true);
+        }
+        else if(Input.GetMouseButtonUp(0) && panel.activeSelf && !(panel.GetComponent<RectTransform>().rect.Contains(Input.mousePosition)))
+        { 
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Main_Camera_Control.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Camera_Control : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0, Space.World);
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.World);
+        }
+    }
+}
