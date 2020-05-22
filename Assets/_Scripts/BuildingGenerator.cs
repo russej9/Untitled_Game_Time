a6dbd45583d2339219804116eb905bf96c099fb5
blaze768 <64864962+blaze768@users.noreply.github.com>
2020-05-22 14:15:56 -0400

    asdj
    asdja

---
+++ Assets/_Scripts/BuildingGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour{
+
+    public FloatVariable playerWood;
+    public FloatVariable playerStone;
+    public FloatVariable playerIron;
+
+    public void BuildButton()
+    {
+        //after click needs to have cursor area for how big building asset is then when another click is made if it is valid area the building gets made
+    }
+
+    private void RemoveResources() //probably pass in parameter of building object to this and below method
+    {
+        
+    }
+
+    public void SpawnBuildingAndAnimation()
+    {
+        RemoveResources();
+        //half built building asset appears
+        //over period of time half built disappears and finished building appears in the same spot 
+    }
+}
---
+++ Assets/_Scripts/ResourceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+
+    public FloatVariable playerWood;
+    public FloatVariable playerStone;
+    public FloatVariable playerIron;
+
+    public string job;
+
+    public void CollectResources()      //player will send an npc to go collect items
+    {
+        if(job == "lumber")
+        {
+            playerWood.value = Random.Range(30f, 55f);
+        }
+        else if(job == "mining")
+        {
+            playerStone.value = Random.Range(30f, 55f);
+            playerIron.value = Random.Range(5f, 15f);
+        }
+    }
+}
