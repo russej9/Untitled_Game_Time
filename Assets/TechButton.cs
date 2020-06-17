c762a94d41c6b472ad2f2597fc72507ad9e858c3
Joe <russej9@mail.uc.edu>
2020-06-17 10:58:23 -0400

    Pathfinding

--- Assets/TechButton.cs
+++ Assets/TechButton.cs
@@ -10,14 +10,7 @@ using UnityEngine.UIElements;
 
 public class TechButton : MonoBehaviour
 {
-
-    private GameObject[] m_techList; //list of all techs, not sure if I will eventually need, may get rid of it
     public GameObject[] m_prereqTech; //list of prerequisit tech, can adjust how many prereq 
-    public Color m_colorLine = Color.white;
-    public float m_lineWidth = 1f;
-    private LineRenderer m_lineRend;
-    public int m_lineSortOrd;
-    public Material m_lineMat;
     public bool m_researched = false;
     private UnityEngine.UI.Button m_researchButton;
     private Color m_buttonColor;
@@ -26,18 +19,6 @@ public class TechButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_techList = GameObject.FindGameObjectsWithTag("Tech");
-        m_lineRend = gameObject.AddComponent<LineRenderer>();
-        for (int i = 0; i < m_prereqTech.Length; i++) //this doesnt work, cant make appear properly on the UI canvas
-        {
-            m_lineRend.SetPosition(0, transform.position);
-            m_lineRend.SetPosition(1, m_prereqTech[i].transform.position);
-            m_lineRend.startWidth = m_lineWidth;
-            m_lineRend.endWidth = m_lineWidth;
-            m_lineRend.startColor = m_colorLine;
-            m_lineRend.endColor = m_colorLine;
-            m_lineRend.sharedMaterial = m_lineMat;
-        }
         //event stuff
         a_research += DoResearch; //the only function of the listener added to the event
         m_researchButton = GetComponent<UnityEngine.UI.Button>();
@@ -47,13 +28,14 @@ public class TechButton : MonoBehaviour
         EventTrigger.Entry buttonHover = new EventTrigger.Entry(); //this allows detection of hovering over button, Use as reference when creating events with triggers
         buttonHover.eventID = EventTriggerType.PointerEnter;
         buttonHover.callback.AddListener((eventData) => { ShowPreReq(); });
+
         EventTrigger.Entry buttonExit = new EventTrigger.Entry();
         buttonExit.eventID = EventTriggerType.PointerExit;
         buttonExit.callback.AddListener((eventData) => { StopShowPreReq(); });
         GameObject.Find(m_researchButton.name).AddComponent<EventTrigger>(); //adds the eventtrigger component to the button
+        
         m_researchButton.GetComponent<EventTrigger>().triggers.Add(buttonHover); //adds the event for showing prereq to the button
         m_researchButton.GetComponent<EventTrigger>().triggers.Add(buttonExit); //adds the event for stopping showing the prereq
-
 
     }
 
@@ -94,14 +76,20 @@ public class TechButton : MonoBehaviour
     {
         for (int i = 0; i < m_prereqTech.Length; i++)
         {
-            m_prereqTech[i].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+            if (m_prereqTech[i].GetComponent<TechButton>().m_researched == false)
+            {
+                m_prereqTech[i].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+            }
         }
     }
     void StopShowPreReq()
     {
         for (int i = 0; i < m_prereqTech.Length; i++)
         {
-            m_prereqTech[i].GetComponent<UnityEngine.UI.Image>().color = m_buttonColor;
+            if (m_prereqTech[i].GetComponent<TechButton>().m_researched == false)
+            {
+                m_prereqTech[i].GetComponent<UnityEngine.UI.Image>().color = m_buttonColor;
+            }
         }
     }
 
